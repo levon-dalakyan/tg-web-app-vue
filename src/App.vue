ae1380ae678915ce5f2ed77bb787e26d840f6df2
levon-dalakyan <wman8800@gmail.com>
2023-03-17 20:57:35 +0300

    initial

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <div><button @click="onClose">Close</button></div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            tg: window.Telegram.WebApp,
+        };
+    },
+    methods: {
+        onClose() {
+            this.tg.close();
+        },
+    },
+    created() {
+        console.log(this.tg);
+    },
+    mounted() {
+        this.tg.ready();
+    },
+};
+</script>
+
+<style scoped>
+.logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+}
+.logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+    filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
